e60cdd586f5e952ea9bc5080ef0a89b0f0206664
xxx <1478531480@qq.com>
2025-03-04 13:51:34 +0800

    第二次作业

---
+++ assignment2/assignment2.3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace assignment2._3
+{
+    internal class Program
+    {
+        static List<int> allPrime(int n)
+        {
+            List<int> result = new List<int>();
+            List<bool> isPrime = Enumerable.Repeat(true, n+1).ToList();  //易错：List<bool> isPrime= new List<bool>(n+1);
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = 2; j * i <= n; j++)
+                        isPrime[j * i] = false;
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2-100以内的素数有：");
+            List<int> result = allPrime(100);
+            foreach(int i in result)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ assignment2/assignment2.4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace assignment2._4
+{
+    internal class Program
+    {
+        static bool IsToeplitzMatrix(int[][]matrix)
+        {
+            int x = matrix.Length;
+            int y = matrix[0].Length;
+            for(int i = 0; i < x-1; i++)
+            {
+                for(int j = 0; j < y-1; j++)
+                {
+                    if (matrix[i][j] != matrix[i+1][j+1])
+                        return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int[][] matrix = new int[][] 
+            {
+                new int[] {1, 2, 3, 4},
+                new int[] {5, 1, 2, 3},
+                new int[] {9, 5, 1, 2}
+            };
+            Console.WriteLine(IsToeplitzMatrix(matrix));
+        }
+    }
+}

040ccab06e5ee6225bd7dde0aea586c4b6203bc5
xxx <1478531480@qq.com>
2025-03-04 11:15:13 +0800

    第二次作业

---
+++ assignment2/assignment2.1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace assignment2._1
+{
+    internal class Program
+    {
+        static Stack<int> OutputPrimeFactors (int num)
+        {
+            Stack<int> output = new Stack<int>();
+            double sqrtNum = Math.Sqrt(num);
+            for (int i=2;i<=sqrtNum;i++)
+            {
+                if(num % i == 0)
+                {
+                    output.Push(i);
+                    while(num % i == 0)
+                        num = num / i;
+                }
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            while (true) 
+            {
+                Console.WriteLine("请输入数据：");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out num)) break;
+                Console.WriteLine("输入数据无效！");
+            }
+            Stack<int> output = OutputPrimeFactors(num);
+            Console.WriteLine("所有素数因子为：");
+            foreach (int item in output) Console.WriteLine(item);
+        }
+    }
+}

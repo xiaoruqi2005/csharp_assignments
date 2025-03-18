de7ab65dd65be5f314e6b6547b90a97225befe02
xxx <1478531480@qq.com>
2025-03-18 22:02:45 +0800

    第四次作业

---
+++ assignment4/assignment4.1/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace assignment4._1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int
+            GenericList<int> intList = new GenericList<int>();
+            for (int x = 1; x <= 10; x++)
+            {
+                intList.Add(x);
+            }
+
+            Console.WriteLine("打印整数链表元素:");
+            intList.ForEach(x => Console.WriteLine(x));
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+
+            intList.ForEach(x => {
+                if (x > max) max = x;
+                if (x < min) min = x;
+                sum += x;
+            });
+
+            Console.WriteLine($"最大值: {max}");
+            Console.WriteLine($"最小值: {min}");
+            Console.WriteLine($"总和: {sum}");
+
+            // string
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 1; x <= 10; x++)
+            {
+                string str = "str" + x;
+                strList.Add(str);
+            }
+
+            Console.WriteLine("打印字符串链表元素:");
+            strList.ForEach(s => Console.WriteLine(s));
+        }
+    }
+ 
+}
---
+++ assignment4/assignment4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+namespace assignment4._2
+{
+    class Clock
+    {
+        public event Action Tick;
+        public event Action Alarm;
+
+        private int alarmTime; 
+
+        public Clock(int time)
+        {
+            alarmTime = time;
+        }
+
+        public void AlarmStart()
+        {
+            int currentTime = 0;
+            while (currentTime <= alarmTime)
+            {
+                Thread.Sleep(1000); 
+                Tick?.Invoke(); 
+                currentTime++;
+                if (currentTime == alarmTime)
+                {
+                    Alarm?.Invoke();
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------开始-------------");
+            //5s示例
+            Clock clock = new Clock(5);
+            clock.Tick += () => Console.WriteLine("滴嗒");
+            clock.Alarm += () => Console.WriteLine("闹钟响了！");
+            clock.AlarmStart();
+        }
+    }
+}

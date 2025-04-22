7c3ba987dc12c776924c7a875f5b05d6dca56abc
xxx <1478531480@qq.com>
2025-04-22 16:30:39 +0800

    第七次作业

---
+++ assignment8/OrderService/Migrations/202504220740054_InitialCreate.cs
@@ -0,0 +1,73 @@
+﻿namespace OrderApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128, storeType: "nvarchar"),
+                        Name = c.String(unicode: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128, storeType: "nvarchar"),
+                        Name = c.String(unicode: false),
+                        Price = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128, storeType: "nvarchar"),
+                        Index = c.Int(nullable: false),
+                        ProductId = c.String(maxLength: 128, storeType: "nvarchar"),
+                        OrderId = c.String(maxLength: 128, storeType: "nvarchar"),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Products", t => t.ProductId)
+                .ForeignKey("dbo.Orders", t => t.OrderId)
+                .Index(t => t.ProductId)
+                .Index(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.String(nullable: false, maxLength: 128, storeType: "nvarchar"),
+                        CustomerId = c.String(maxLength: 128, storeType: "nvarchar"),
+                        CreateTime = c.DateTime(nullable: false, precision: 0),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Customers", t => t.CustomerId)
+                .Index(t => t.CustomerId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderDetails", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Orders", "CustomerId", "dbo.Customers");
+            DropForeignKey("dbo.OrderDetails", "ProductId", "dbo.Products");
+            DropIndex("dbo.Orders", new[] { "CustomerId" });
+            DropIndex("dbo.OrderDetails", new[] { "OrderId" });
+            DropIndex("dbo.OrderDetails", new[] { "ProductId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.Products");
+            DropTable("dbo.Customers");
+        }
+    }
+}
